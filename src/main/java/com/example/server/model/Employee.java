8815cc3762311a871d17e2606d7467add0566c7c
grpriya <44578345+grpriya@users.noreply.github.com>
2018-10-31 11:08:56 -0700

    Update Employee.java

--- src/main/java/com/example/server/model/Employee.java
+++ src/main/java/com/example/server/model/Employee.java
@@ -1,31 +1,65 @@
 package com.example.server.model;
 
-public class Employee {
-    private int id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table
+public class Employee implements Serializable {
+
+    private String empId;
     private String firstName;
+    private String lastName;
+    private String email;
 
     public Employee() {
-
     }
 
-    public Employee(int id, String firstName) {
-        this.id = id;
+    public Employee(String empId, String firstName, String lastName, String email) {
+        this.empId = empId;
         this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
     }
 
-    public int getId() {
-        return id;
+    @Id
+    @Column(name="empId")
+    public String getEmpId() {
+        return empId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setEmpId(String empId) {
+        this.empId = empId;
     }
 
+    @Column(name="firstName")
     public String getFirstName() {
         return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    @Column(name="lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name="email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
